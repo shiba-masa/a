ea2d028294d370d9fcd53ac8c5e054653a657e9c
shiba <haru.shiba@icloud.com>
2017-04-28 12:36:20 +0900

    ラベル入れた

--- a/ViewController.swift
+++ a/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let label = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel()
+        label.text = "aaa"
+        label.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        self.view.addSubview(label)
+        self.view.bringSubview(toFront: label)
     }
 
     override func didReceiveMemoryWarning() {
